76c4f8f144f3a2dc9379bf381a70001e081213f1
Jeyran999 <ceyranguliyeva2006@gmail.com>
2026-08-04 17:07:24 +0400

    Week-3 Checkpoint - 6: add transaction rollback unit tests

--- src/tests/book.service.test.js
+++ src/tests/book.service.test.js
@@ -1,9 +1,27 @@
+const mongoose = require("mongoose");
 const bookService = require("../services/book.service");
 const bookRepository = require("../repositories/book.repository");
+const authorRepository = require("../repositories/author.repository");
+const categoryRepository = require("../repositories/category.repository");
+const tagRepository = require("../repositories/tag.repository");
 
+jest.mock("mongoose");
 jest.mock("../repositories/book.repository");
+jest.mock("../repositories/author.repository");
+jest.mock("../repositories/category.repository");
+jest.mock("../repositories/tag.repository");
 
 describe("Book Service", () => {
+  const mockSession = {
+    startTransaction: jest.fn(),
+    commitTransaction: jest.fn(),
+    abortTransaction: jest.fn(),
+    endSession: jest.fn(),
+  };
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mongoose.startSession.mockResolvedValue(mockSession);
+  });
   test("should return a book by id", async () => {
     const mockBook = {
       _id: "123",
@@ -24,5 +42,54 @@ describe("Book Service", () => {
     const result = await bookService.getBookById("invalid-id");
 
     expect(result).toBeNull();
+  });
+
+  test("should rollback transaction if an error occurs", async () => {
+    const bookData = {
+      title: "Clean Code",
+      author: "author-id",
+      category: "category-id",
+      tags: ["tag-id"],
+    };
+    const mockBook = {
+      _id: "book-id",
+      ...bookData,
+    };
+
+    const mockAuthor = {
+      _id: "author-id",
+      books: [],
+    };
+
+    const mockCategory = {
+      _id: "category-id",
+      books: [],
+    };
+
+    const mockTag = {
+      _id: "tag-id",
+      books: [],
+    };
+
+    bookRepository.create.mockResolvedValue(mockBook);
+
+    authorRepository.findById.mockResolvedValue(mockAuthor);
+    authorRepository.save.mockResolvedValue(mockAuthor);
+
+    categoryRepository.findById.mockResolvedValue(mockCategory);
+    categoryRepository.save.mockResolvedValue(mockCategory);
+
+    tagRepository.findById.mockResolvedValue(mockTag);
+
+    tagRepository.save.mockRejectedValue(new Error("Database error"));
+
+    await expect(bookService.createBook(bookData)).rejects.toThrow(
+      "Database error",
+    );
+
+    expect(mockSession.startTransaction).toHaveBeenCalled();
+    expect(mockSession.abortTransaction).toHaveBeenCalled();
+    expect(mockSession.commitTransaction).not.toHaveBeenCalled();
+    expect(mockSession.endSession).toHaveBeenCalled();
   });
 });
