cf2f3d08ba1a64e3aebb1c3f4a0fc4d83e3d5024
Jeyran999 <ceyranguliyeva2006@gmail.com>
2026-08-10 13:00:03 +0400

    Week-4 Checkpoint - 6:  add environment-based configuration

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-require("dotenv").config()
+const dotenv = require("dotenv");
 
-const app = require("./src/app")
-const connectDB = require("./src/config/database")
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+
+dotenv.config({ path: envFile });
+
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
 const { startCacheCleanupJob } = require("./src/schedulers/cache.scheduler");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const startServer = async () => {
-    await connectDB();
+  await connectDB();
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
 startCacheCleanupJob();
 
 startServer();
